a2652ffc6926b4eeb4fa3c3bf03ef376ae75507a
Victor-02 <victorcasemiro2002@hotmail.com>
2022-02-09 15:31:06 -0400

    Tests and Adjustment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,38 @@
 package application;
 
+import java.util.Date;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller sel = sellerDao.findById(3);
+		Department dep = new Department(6, null);
 
+		System.out.println("---Test findById---");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller sel = sellerDao.findById(7);
 		System.out.println(sel);
+
+		System.out.println("\n---Test update---");
+		sel = sellerDao.findById(10);
+		sel.setName("Jhon");
+		sellerDao.update(sel);
+		System.out.println("Updated!");
+
+		System.out.println("\n---Test deleteById---");
+		sellerDao.deleteById(9);
+		System.out.println("Deleted!");
+
+		System.out.println("\n---Test insert---");
+		Seller newsel = new Seller(null, "Victor", "victor@victor.com", new Date(), 2000.00, dep);
+		sellerDao.insert(newsel);
+		System.out.println("Insert Complete!");
+
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
+import java.sql.Statement;
 import java.util.List;
 
 import db.DB;
@@ -24,26 +24,32 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public void insert(Seller sel) {
 		PreparedStatement st = null;
-		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES (?, ?, ?, ?, ?)");
-			st.setString(1, "Victor");
-			st.setString(2, "victor@victor.com");
-			st.setDate(3, (java.sql.Date) new Date("16/06/2002"));
-			st.setDouble(4, 1000.00);
-			st.setInt(5, 1);
+			st = conn.prepareStatement(
+					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES (?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, sel.getName());
+			st.setString(2, sel.getEmail());
+			st.setDate(3, new java.sql.Date(sel.getBirthDate().getTime()));
+			st.setDouble(4, sel.getBaseSalary());
+			st.setInt(5, sel.getDepartment().getId());
 
-			if (rs.next()) {
-				System.out.println("Seller Created!");
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					sel.setId(id);
+				}
+				DB.closeResultSet(rs);
 			} else {
-				System.out.println("Error!");
+				throw new DbException("Unexpected Error! No rows affected! ");
 			}
 
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
 	}
@@ -51,20 +57,22 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public void update(Seller sel) {
 		PreparedStatement st = null;
-		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("UPDATE seller " + "WHERE seller.Id = ? " + "SET Name = ?");
-			st.setInt(1, 2);
-			st.setString(2, "Jhon");
-			if (rs.next()) {
-				System.out.println("ID updated!");
-			} else {
-				System.out.println("ID not found!");
-			}
+			st = conn.prepareStatement(
+					"UPDATE seller " + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+							+ "WHERE seller.Id = ?");
+			st.setString(1, sel.getName());
+			st.setString(2, sel.getEmail());
+			st.setDate(3, new java.sql.Date(sel.getBirthDate().getTime()));
+			st.setDouble(4, sel.getBaseSalary());
+			st.setInt(5, sel.getDepartment().getId());
+			st.setInt(6, sel.getId());
+
+			st.executeUpdate();
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
-			DB.closeStatement(st);
 			DB.closeStatement(st);
 		}
 
@@ -73,20 +81,15 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public void deleteById(Integer id) {
 		PreparedStatement st = null;
-		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("DELETE FROM seller WHERE seller.Id = ?");
-			st.setInt(1, 2);
-			rs = st.executeQuery();
-			if (rs.next()) {
-				System.out.println("ID deleted!");
-			} else {
-				System.out.println("ID not found!");
-			}
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			st.setInt(1, id);
+
+			st.executeUpdate();
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
 	}
@@ -126,7 +129,7 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-
+		return null;
 	}
 
 }
